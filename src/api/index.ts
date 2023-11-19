823205c36ec3592dc6382576aaaeaa95b0c571c2
ALL <all@example.com>
2023-11-19 20:54:17 +0800

    fix: api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,4 @@
-/**
- * 通用
- */
 export * from '@/api/common';
-
-/**
- * 账单
- */
 export * from '@/api/bill';
-
-/**
- * 用户
- */
-export * from 'src/api/system/user';
-
-/**
- * 消费类型
- */
-export * from 'src/api/system/type';
+export * from '@/api/system/user';
+export * from '@/api/system/type';
